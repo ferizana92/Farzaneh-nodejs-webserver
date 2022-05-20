025f48aedc20dd9d0f1f114c8cdf6b54f3778391
Farzaneh <43077286+ferizana92@users.noreply.github.com>
2022-05-20 18:59:01 +0430

    fix

--- index.js
+++ index.js
@@ -3,57 +3,53 @@ const port = 5000;
 const fs = require('fs')
 const path = require('path');
 
-const server =http.createServer((request, response)=>
-{
-    let filePath = path.join(
-        __dirname,
-        './view',
-        req.url==='./' ? "index.html" : request.url
-    );
 
-    let extensionname = path.extensionname(filePath)
-    let contentType ='text/html';
+const server=http.createServer((request, response) =>{
+    let extentionname = "." + request.url
+    if (extensionname=== "./" ){
+        fs.readFile("index.html", (error,value)=>{
+            if (error){
+                response.writeHead( 404 , { "content-type" : "text/html"})
 
-    switch(extensionname){
-        case ".js" :
-            contentType="text/javascript";
-            break;
-        case ".css" :
-            contentType="text/css";
-            break;
-        case ".png" :
-            contentType="image/png";
-            break;
-    }
-
-if (contentType==="text/html" && extensionname==='' ){
-    filePath+= '.html' ;
-}
-
-fs.readFile(filePath,
-    (error , data) =>{
-        if (error)
-            {
-                console.log('something went wrong', err)
+                response.end("something went wrong")
             }
             else{
-                res.writehead(200 , {'content-type' : contentType} );
-                res.end('data'); 
+                response.writeHead( 200 , { "content-type" : "text/html"})
+                response.write(value)
+                response.end ()
+
             }
+
+
+        })
+
     }
-    )
+    else
+    {
+        fs.readFile(extentionname, (error,value)=>{
+            if (error){
+                response.writeHead( 404 , { "content-type" : "text/html"})
+
+                response.end("something went wrong")
+            }
+            else{
+                
+            }
+response.write(value)
+response.end(value)
+
+        });
+
+    }
 });
 
-server.listen(port, function(error)
-{
-    if (error)
-    {
-        console.log('something went wrong', err)
+
+
+server.listen(port, (err)=>{
+    if (err){
+        console.log('error')
     }
     else{
-        // res.writehead(200 'content-type' 'text/html' )
-        // res.writehead(200 'content-type' 'text/css' )
-
+        console.log('listening port 5000')
     }
-
 })
