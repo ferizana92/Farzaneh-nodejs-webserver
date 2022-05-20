d701c824611038ecb13d0d9cd048c2c0548fb87d
Farzaneh <43077286+ferizana92@users.noreply.github.com>
2022-05-20 19:09:25 +0430

    fix

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const port = 5000;
 const fs = require('fs')
 const path = require('path');
 
-
 const server=http.createServer((request, response) =>{
     let extentionname = "." + request.url
     if (extensionname=== "./" ){
@@ -20,7 +19,6 @@ const server=http.createServer((request, response) =>{
 
             }
 
-
         })
 
     }
@@ -33,17 +31,21 @@ const server=http.createServer((request, response) =>{
                 response.end("something went wrong")
             }
             else{
+                switch(path.extname(filePath))
+                {
+                    case ".html": res.writeHead(200, {"Content-Type": "text/html"});
+                    break;
+                    case ".css": res.writeHead(200, {"Content-Type": "text/css"});
+                    break;
+                    case ".png": res.writeHead(200, {"Content-Type": "image/png"});
+                    break;
                 
             }
-response.write(value)
-response.end(value)
 
         });
 
     }
 });
-
-
 
 server.listen(port, (err)=>{
     if (err){
@@ -53,3 +55,62 @@ server.listen(port, (err)=>{
         console.log('listening port 5000')
     }
 })
+
+
+
+
+// const server =http.createServer((request, response)=>
+
+// {
+//     let filePath = path.join(
+//         __dirname,
+//         './view',
+//         req.url==='./' ? "index.html" : request.url
+//     );
+
+//     let extensionname = path.extensionname(filePath)
+//     let contentType ='text/html';
+
+//     switch(extensionname){
+//         case ".js" :
+//             contentType="text/javascript";
+//             break;
+//         case ".css" :
+//             contentType="text/css";
+//             break;
+//         case ".png" :
+//             contentType="image/png";
+//             break;
+//     }
+
+// if (contentType==="text/html" && extensionname==='' ){
+//     filePath+= '.html' ;
+// }
+
+// fs.readFile(filePath,
+//     (error , data) =>{
+//         if (error)
+//             {
+//                 console.log('something went wrong', err)
+//             }
+//             else{
+//                 res.writehead(200 , {'content-type' : contentType} );
+//                 res.end('data'); 
+//             }
+//     }
+//     )
+// });
+
+// server.listen(port, function(error)
+// {
+//     if (error)
+//     {
+//         console.log('something went wrong', err)
+//     }
+//     else{
+//         // res.writehead(200 'content-type' 'text/html' )
+//         // res.writehead(200 'content-type' 'text/css' )
+
+//     }
+
+// })
